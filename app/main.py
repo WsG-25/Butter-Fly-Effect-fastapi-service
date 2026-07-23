3cd2554a3a829fdc38ebb17834e836d5474d1a8f
chawkins-stack <chawkins@catalyte.io>
2026-07-23 10:30:16 -0500

    changes chamar

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 
+#Creating a class instance for fastapi
 app = FastAPI()
+
+#Creating a root route
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
+#Creating a hello route
+#This route will take a name as a parameter and return a message with the name
+
+
 @app.get("/hello/{name}")
 def hello(name: str):
     return {"message": f"Hello, {name}!"}
+
+# FastAPI will convert the language to Postman for JSON formatting.
