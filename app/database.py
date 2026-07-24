7b26d4dbce358866de9ce9f70c1fcf853bb2b47c
Pooja Tanneru <ptanneru@catalyte.io>
2026-07-24 12:33:17 -0500

    Modified databse.py

--- app/database.py
+++ app/database.py
@@ -1,20 +1,20 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import DeclarativeBase
-
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 class Base(DeclarativeBase):
       pass
 
 DATABASE_URL = ("postgresql://postgres:root@localhost:5432/Butterfly-effect")
 
-engine = create_engine("postgresql://postgres:root@localhost:5432/Butterfly-effect")
+# Create the database engine
+engine = create_engine(DATABASE_URL)
 
+# Create database sessions
 SessionLocal = sessionmaker(
     autoflush=False,
     autocommit=False,
     bind=engine
 )
 
-# Base class every SQLAlchemy model inherits from
+# Base class for all models
 Base = DeclarativeBase()
