b34091680df1dfc6fe85ea1daf29f9d2afa4a7ea
Pooja Tanneru <ptanneru@catalyte.io>
2026-07-28 12:53:57 -0500

    Database Port

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 
-DATABASE_URL = "postgresql+psycopg2://postgres:root@localhost:5432/Butterfly-effect"
+DATABASE_URL = "postgresql+psycopg2://postgres:root@localhost:5433/Butterfly-effect"
 
 engine = create_engine(DATABASE_URL)
 
