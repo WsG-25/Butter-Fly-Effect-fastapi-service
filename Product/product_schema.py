0cc47b3d39f32152db14b36475419fd4cf35832d
Worst Generation <cwilson@catalyte.io>
2026-07-27 19:57:54 -0500

    added new format

--- Product/product_schema.py
+++ Product/product_schema.py
@@ -2,12 +2,11 @@ from pydantic import BaseModel, Field
 
 
 class ProductBase(BaseModel):
-    id : int
     name: str
     unit: str
     cost_per_unit: float = Field(gt=0)
     price_per_unit: float = Field(gt=0)
-    quantity_in_stock: float = Field(ge=0)
+    quantity_in_stock: int = Field(ge=0)
 
 class ProductCreate(ProductBase):
     pass
