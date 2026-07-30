6fc55e7bd1eb68c66d6e6d01f3951747a06fd79f
Worst Generation <cwilson@catalyte.io>
2026-07-30 13:41:45 -0500

    commit Category change

--- Category/category_model.py
+++ Category/category_model.py
@@ -1,4 +1,3 @@
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 
@@ -6,12 +5,17 @@ from database import Base
 
 
 class Category(Base):
+
     __tablename__ = "categories"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False, unique=True)
 
-    # One category can have many products
+    name = Column(
+        String,
+        nullable=False,
+        unique=True
+    )
+
     products = relationship(
         "Product",
         back_populates="category"
